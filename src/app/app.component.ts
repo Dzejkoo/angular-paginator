273df4612d2cf3bf4e7a472444d9ed73449f2577
Jakub Jóźwiak <jozwiak.ja@icloud.com>
2022-12-11 20:34:13 +0100

    Added full setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { UsersService } from './data.service';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pagination-angular';
+  displayColumns = ['id', 'title'];
+  dataSource!: MatTableDataSource<any>;
+  paginatorOptions = {
+    length: 0,
+    pageIndex: 0,
+    pageSize: 10,
+    pageSizeOption: [10, 20, 30, 40],
+  };
+  @ViewChild('paginator') paginator!: MatPaginator;
+
+  paginatorChange(event: PageEvent): void {
+    this.paginatorOptions.pageSize = event.pageSize;
+
+    this.getData();
+  }
+
+  constructor(private usersService: UsersService) {}
+  ngOnInit(): void {
+    console.log(this.paginatorOptions.pageSize);
+    this.getData();
+  }
+  getData() {
+    this.usersService.getLogs().subscribe((res: any) => {
+      this.dataSource = new MatTableDataSource(res);
+      this.dataSource.paginator = this.paginator;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatPaginatorModule,
+    MatTableModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
